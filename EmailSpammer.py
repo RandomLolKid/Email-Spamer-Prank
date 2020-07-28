45a0dea4aa9d69de3628008ac882ca0d4ad71558
RandomLolKid <68888559+RandomLolKid@users.noreply.github.com>
2020-07-28 15:59:01 +1000

    The Main Program!

--- EmailSpammer.py
+++ EmailSpammer.py
@@ -1,5 +1,6 @@
-import smtplib 
-import getpass
+#LOL Prank ur friend with this :)
+
+import smtplib
 from email.mime.multipart import MIMEMultipart 
 from email.mime.text import MIMEText 
 from email.mime.base import MIMEBase 
